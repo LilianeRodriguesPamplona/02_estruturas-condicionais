600ba8c106002621a73c3736d4b04199c8b2ea4d
LilianeRodriguesPamplona <liliane.rodriguesssz@gmail.com>
2024-01-14 20:48:02 -0300

    02_estruturas-condicionais

---
+++ aula.js
@@ -0,0 +1,36 @@
+/*
+const numero = 1;
+
+const NumeroPar = (numero % 2) === 0;
+
+if (NumeroPar) {
+    console.log('Par');
+} else {
+    console.log('Impar');
+}
+*/
+
+/*
+const numero = 5;
+
+const NumeroDivisivelPor5 = (numero % 5) === 0;
+
+if (numero === 0) {
+    console.log('O número é inválido');
+} else if (numero === 5) {
+    console.log('O número é inválido');
+} else if (NumeroDivisivelPor5) {
+    console.log('Sim!');
+} else {
+    console.log('Não!');
+}
+*/
+
+const numero = 5;
+const isNumeroPar = (numero % 2) === 0;
+
+if (isNumeroPar) {
+    console.log('O número é par');
+} else {
+    console.log('O número é impar');
+}
